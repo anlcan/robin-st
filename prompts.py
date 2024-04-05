78bc96464ec0554c8784ed1c26dcf62e537e26cc
Anil Can Baykal <anilcan.baykal@kfzteile24.de>
2024-04-05 15:42:15 +0200

    Refactored prompts to include instructions for generating text in German with specific formatting requirements.

--- prompts.py
+++ prompts.py
@@ -10,9 +10,14 @@ prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "Write a summary of the following in 200-250 words in B2 German level break it into",
+            """
+            Write a summary of the following text in 200-250 words in {level} German level.
+            Break the text into paragraphs.
+            The output text will be rendered in html so wrap the paragraphs in <p> tags. 
+            At the end of the text, list any domain specific words that you used in the text.
+                """,
         ),
-        ("system", "the output text will be rendered in html so wrap the paragraps in <p> tags"),
+        ("system", ""),
         ("user", "{text}"),
     ]
 )
@@ -22,19 +27,22 @@ prompt_exercise = ChatPromptTemplate.from_messages(
         (
             "system",
             """
-    You are an excellent German teacher, and you generate exercises for your students.
+    You are an excellent German teacher, and you generate fill-in-the-blanks type exercises for your students.
     You always create exercises that are challenging and engaging for your students.
-    You create exercises that are tailored to the level of your students, which is {level} in this case. 
+    You create exercises that are tailored to the CEFR level of your students. 
+    The level is {level} in this case. 
     You keep the sentences challenging but understandable, use the vocabulary that is appropriate for the level.
+    You can add one sentence before after the exercise to give context.
     you generate one sentence per exercise unless specified otherwise. 
     The exercises are fill-in-the-blank exercises.
     The text generated should be in German.
     The text generated should be in the CSV format, where first colum is the exercise, the second colum is the correct answer, 
-    and add three columns with wrong answers. use | as the delimiter.
+    and add three columns with wrong answers. 
+    use | as the delimiter.
     Mark the blank with dashes: ___
     don't start with the column description
 
-    Here is a bad example, becuase it is short and lacks context:
+    Here is a bad example, because it is short and lacks context:    
     Ich ___ am Wochenende ins Kino. | gehe | gehen | geht | gegangen
 
     Here area a good example:
@@ -43,7 +51,7 @@ prompt_exercise = ChatPromptTemplate.from_messages(
 
     """,
         ),
-        ("user", "create an exercise on the following topic: {text}"),
+        ("user", "create an fill-in-the-blanks exercise on the following topic: {text}"),
     ]
 )
 
@@ -56,3 +64,8 @@ def get_prompt_chain(openai_api_key=OPENAI_API_KEY):
 def get_prompt_exercise_chain(openai_api_key=OPENAI_API_KEY):
     llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
     return prompt_exercise | llm | StrOutputParser()
+
+
+def get_chain(prompt):
+    llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+    return prompt | llm | StrOutputParser()
