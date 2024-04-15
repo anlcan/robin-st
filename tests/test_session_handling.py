3b53512afca50b8c2b7b642aaa0e95f422290404
Anil Can Baykal <anilcan.baykal@kfzteile24.de>
2024-04-15 07:58:04 +0200

    tests

---
+++ tests/test_session_handling.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestBasicVerifier(TestCase):
+    def test_identifier(self):
+        self.fail()
+
+    def test_backend(self):
+        self.fail()
+
+    def test_auto_error(self):
+        self.fail()
+
+    def test_auth_http_exception(self):
+        self.fail()
+
+    def test_verify_session(self):
+        self.fail()
+
+    def test_create_session(self):
+        self.fail()
